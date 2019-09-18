6f2c9e3dcefd53c4695c296728560a2a70ea105f
unknown <prabir.k@wuzardcomm.net>
2019-09-18 18:28:54 +0530

    Add Header Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bwm-ng';
+  title = 'I am component title';
+  clickHandler(){
+    alert(1);
+  }
 }
